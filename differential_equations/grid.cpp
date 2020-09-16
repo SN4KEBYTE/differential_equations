ef10da48665c2e3a42026467ad78914d19359a75
SN4KEBYTE <timurcat7@gmail.com>
2020-09-16 15:10:30 +0700

    Codestyle fixes.

--- differential_equations/grid.cpp
+++ differential_equations/grid.cpp
@@ -2,10 +2,10 @@
 
 vector<double> build_grid(const double &a, const double &step, const int &N)
 {
-   vector<double> grid(N + 1);
+    vector<double> grid(N + 1);
 
-   for (size_t i = 0; i < N + 1; i++)
-      grid[i] = a + i * step;
+    for (size_t i = 0; i < N + 1; i++)
+        grid[i] = a + i * step;
 
-   return grid;
+    return grid;
 }
--- differential_equations/solvers.cpp
+++ differential_equations/solvers.cpp
@@ -2,23 +2,23 @@
 
 vector<double> simple_explicit_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
 {
-   vector<double> y(N + 1);
-   y[0] = y0;
+    vector<double> y(N + 1);
+    y[0] = y0;
 
-   for (size_t i = 1; i < y.size(); i++)
-      y[i] = y[i - 1] + step * f(grid[i], y[i - 1]);
+    for (size_t i = 1; i < y.size(); i++)
+        y[i] = y[i - 1] + step * f(grid[i], y[i - 1]);
 
-   return y;
+    return y;
 }
 
 vector<double> modified_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
 {
-   // your implementation here...
-   return vector<double>();
+    // your implementation here...
+    return vector<double>();
 }
 
 vector<double> improved_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
 {
-   // your implementation here...
-   return vector<double>();
+    // your implementation here...
+    return vector<double>();
 }
