1955bcfbd5ea30a3f4eb8d2c33f55de4f5e8a8e2
IIMoJoII <50038179+IIMoJoII@users.noreply.github.com>
2020-09-18 18:22:31 +0700

    Update solvers.cpp

--- differential_equations/solvers.cpp
+++ differential_equations/solvers.cpp
@@ -20,10 +20,10 @@ vector<double> modified_euler(const vector<double> &grid, double(&f)(const doubl
 vector<double> improved_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
 {
     vector<double> z(N + 1);
-	z[0] = z0;
+    z[0] = z0;
 
-	for (size_t i = 1; i < z.size(); i++)
-		z[i] = z[i - 1] + step * f(grid[i+1/2], z[i - 1]+(step/2)*f(grid[i], z[i - 1]));
+    for (size_t i = 1; i < z.size(); i++)
+	z[i] = z[i - 1] + step * f(grid[i+1/2], z[i - 1]+(step/2)*f(grid[i], z[i - 1]));
 
-	return z;
+    return z;
 }
