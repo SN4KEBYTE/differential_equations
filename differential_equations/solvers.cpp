261b1beb9985ed3d73a4037ddc62a427efc24b46
SN4KEBYTE <timurcat7@gmail.com>
2020-09-19 13:58:14 +0700

    Codestyle fixes. Renamed variables and arguments.

--- differential_equations/solvers.cpp
+++ differential_equations/solvers.cpp
@@ -22,13 +22,13 @@ vector<double> modified_euler(const vector<double> &grid, double(&f)(const doubl
     return y;
 }
 
-vector<double> improved_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &z0, const double &step, const int &N)
+vector<double> improved_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
 {
-    vector<double> z(N + 1);
-    z[0] = z0;
+    vector<double> y(N + 1);
+    y[0] = y0;
 
-    for (size_t i = 1; i < z.size(); i++)
-	z[i] = z[i - 1] + step * f(grid[i+1/2], z[i - 1]+(step/2)*f(grid[i], z[i - 1]));
+    for (size_t i = 1; i < y.size(); i++)
+        y[i] = y[i - 1] + step * f(grid[i + 1 / 2], y[i - 1] + (step / 2)*f(grid[i], y[i - 1]));
 
-    return z;
+    return y;
 }
