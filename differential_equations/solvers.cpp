195b8045ee56b30f9c1aac0d0d580d39ea0f6fad
Io256 <71480966+Io256@users.noreply.github.com>
2020-09-19 13:32:54 +0700

    modified_euler added

--- differential_equations/solvers.cpp
+++ differential_equations/solvers.cpp
@@ -6,15 +6,20 @@ vector<double> simple_explicit_euler(const vector<double> &grid, double(&f)(cons
     y[0] = y0;
 
     for (size_t i = 1; i < y.size(); i++)
-        y[i] = y[i - 1] + step * f(grid[i], y[i - 1]);
+        y[i] = y[i - 1] + step * f(grid[i - 1], y[i - 1]);
 
     return y;
 }
 
 vector<double> modified_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
 {
-    // your implementation here...
-    return vector<double>();
+    vector<double> y(N + 1);
+    y[0] = y0;
+
+    for (size_t i = 1; i < y.size(); i++)
+        y[i] = y[i - 1] + (step / 2) * (f(grid[i - 1], y[i - 1]) + f(grid[i], y[i - 1] + (step / 2) * f(grid[i - 1], y[i - 1])));
+
+    return y;
 }
 
 vector<double> improved_euler(const vector<double> &grid, double(&f)(const double &, const double &), const double &y0, const double &step, const int &N)
