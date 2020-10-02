099989cd464faf5c80db782b3102050b8d2f6965
SN4KEBYTE <timurcat7@gmail.com>
2020-10-03 01:48:55 +0700

    Fixed linker error. Utils is a static library now.

--- euler_explicit/solvers.cpp
+++ euler_explicit/solvers.cpp
@@ -28,7 +28,7 @@ vector<double> improved(const vector<double> &grid, double(&f)(const double &, c
     y[0] = y0;
 
     for (size_t i = 1; i < y.size(); i++)
-        y[i] = y[i - 1] + step * f(grid[i] + step / 2, y[i - 1] + (step / 2) * f(grid[i - 1], y[i - 1]));
+        y[i] = y[i - 1] + step * f(grid[i - 1] + step / 2, y[i - 1] + (step / 2) * f(grid[i - 1], y[i - 1]));
 
     return y;
 }
