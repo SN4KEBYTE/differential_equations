dedc09cd3d0180712952d163eaff9585903f6866
SN4KEBYTE <timurcat7@gmail.com>
2020-10-24 02:46:07 +0700

    Added convergence research.

--- test_runner/main.cpp
+++ test_runner/main.cpp
@@ -21,9 +21,6 @@ using namespace std;
 const double EPS = 1e-14;
 const double MAX_ITER = 1000;
 
-// root directory for resulting tables 
-const string RES_DIR = "result/";
-
 #pragma endregion
 
 #pragma region initial research data
@@ -67,54 +64,48 @@ double i_solution(const double &t)
 
 #pragma endregion
 
-//// task conditions
-//const double A = 0., B = 2., Y0 = 1.;
-//
-//// steps
-//const vector<double> simple_steps = { 0.1, 0.05, 0.025 };
-//const vector<double> modified_steps = { 0.2, 0.1, 0.05 };
-//const vector<double> rk_steps = { 0.5, 0.25, 0.125, 0.1 };
-//
-//// number of tests
-//const size_t SIMPLE_TEST_NUM = simple_steps.size();
-//const size_t MOD_TEST_NUM = modified_steps.size();
-//const size_t RK_TEST_NUM = rk_steps.size();
-//
-//// root directory for resulting tables 
-//const string RES_DIR = "result/";
-//
-//// directories for each type of methods
-//const string SIMPLE_DIR = RES_DIR + "simple/";
-//const string MOD_DIR = RES_DIR + "modified/";
-//const string RK_DIR = RES_DIR + "rk/";
-//
-//// function
-//double f(const double &t, const double &y)
-//{
-//    return -25 * y + cos(t) + 25 * sin(t);
-//}
-//
-//// analytical solution
-//double solution(const double &t)
-//{
-//    return exp(-25 * t) + sin(t);
-//}
-//
-//double f2(const double &t, const double &y)
-//{
-//    return 2 * t * y;
-//}
-//
-//double df2(const double &t, const double &y)
-//{
-//    return 2 * y;
-//}
-//
-//// analytical solution
-//double solution2(const double &t)
-//{
-//    return exp(t * t);
-//}
+#pragma region convergence research data
+
+// task conditions
+const double C_A = 0., C_B = 2., C_Y0 = 1.;
+
+// steps
+const vector<double> C_SIMPLE_STEPS = { 0.1, 0.05, 0.025 };
+const vector<double> C_MODIFIED_STEPS = { 0.2, 0.1, 0.05 };
+const vector<double> C_RK_STEPS = { 0.5, 0.25, 0.125, 0.1 };
+
+// number of tests
+const size_t SIMPLE_TEST_NUM = C_SIMPLE_STEPS.size();
+const size_t MOD_TEST_NUM = C_MODIFIED_STEPS.size();
+const size_t RK_TEST_NUM = C_RK_STEPS.size();
+
+// root directory for resulting tables 
+const string CONV_DIR = "convergence_result/";
+
+// directories for each type of methods
+const string SIMPLE_DIR = CONV_DIR + "simple/";
+const string MOD_DIR = CONV_DIR + "modified/";
+const string RK_DIR = CONV_DIR + "rk/";
+
+// function
+double c_f(const double &t, const double &y)
+{
+    return -25 * y + cos(t) + 25 * sin(t);
+}
+
+// function first derivative by y
+double c_df(const double &t, const double &y)
+{
+    return -25.;
+}
+
+// analytical solution
+double c_sol(const double &t)
+{
+    return exp(-25 * t) + sin(t);
+}
+
+#pragma endregion
 
 int main()
 {
@@ -175,80 +166,89 @@ int main()
         out.open(I_PC_DIR + end);
         dump_table(out, grid, pc, as);
         out.close();
+
+        // implicit Euler
+        auto imp = implicit(grid, i_f, i_df, I_Y0, I_STEPS[i], N, EPS, MAX_ITER);
+        out.open(I_EU_IMP_DIR + end);
+        dump_table(out, grid, imp, as);
+        out.close();
+    }
+#pragma endregion
+
+#pragma region convergence research
+    _mkdir(CONV_DIR.c_str());
+
+    _mkdir(SIMPLE_DIR.c_str());
+    _mkdir((SIMPLE_DIR + "explicit/").c_str());
+    _mkdir((SIMPLE_DIR + "implicit/").c_str());
+
+    _mkdir(MOD_DIR.c_str());
+    _mkdir((MOD_DIR + "modified/").c_str());
+    _mkdir((MOD_DIR + "trapeze/").c_str());
+    
+    _mkdir(RK_DIR.c_str());
+
+    // SIMPLE
+    for (size_t i = 0; i < SIMPLE_TEST_NUM; i++)
+    {
+        int N = (C_B - C_A) / C_SIMPLE_STEPS[i];
+        auto grid = build_grid(C_A, C_SIMPLE_STEPS[i], N);
+        auto as = analytical_solution(grid, c_sol);
+
+        auto y_simple = simple(grid, c_f, C_Y0, C_SIMPLE_STEPS[i], N);
+        auto y_implicit = implicit(grid, c_f, c_df, C_Y0, C_SIMPLE_STEPS[i], N, EPS, MAX_ITER);
+
+        auto end = "h" + to_string(C_SIMPLE_STEPS[i]) + ".csv";
+
+        ofstream out(SIMPLE_DIR + "explicit/" + end);
+        out.imbue(locale(""));
+        dump_table(out, grid, y_simple, as);
+        out.close();
+
+        out.open(SIMPLE_DIR + "implicit/" + end);
+        dump_table(out, grid, y_implicit, as);
+        out.close();
+    }
+    
+    // MODIFIED
+    for (size_t i = 0; i < MOD_TEST_NUM; i++)
+    {
+        int N = (C_B - C_A) / C_MODIFIED_STEPS[i];
+        auto grid = build_grid(C_A, C_MODIFIED_STEPS[i], N);
+        auto as = analytical_solution(grid, c_sol);
+
+        auto y_mod = modified(grid, c_f, C_Y0, C_MODIFIED_STEPS[i], N);
+        auto y_trapeze = trapeze(grid, c_f, c_df, C_Y0, C_MODIFIED_STEPS[i], N, EPS, MAX_ITER);
+
+        auto end = "h" + to_string(C_MODIFIED_STEPS[i]) + ".csv";
+
+        ofstream out(MOD_DIR + "modified/" + end);
+        out.imbue(locale(""));
+        dump_table(out, grid, y_mod, as);
+        out.close();
+
+        out.open(MOD_DIR + "trapeze/" + end);
+        dump_table(out, grid, y_trapeze, as);
+        out.close();
     }
 
-    //_mkdir(RES_DIR.c_str());
+    // RUNGE - KUTTA
+    for (size_t i = 0; i < RK_TEST_NUM; i++)
+    {
+        int N = (C_B - C_A) / C_RK_STEPS[i];
+        auto grid = build_grid(C_A, C_RK_STEPS[i], N);
+        auto as = analytical_solution(grid, c_sol);
 
-    //_mkdir(SIMPLE_DIR.c_str());
-    //_mkdir((SIMPLE_DIR + "simple/").c_str());
-    //_mkdir((SIMPLE_DIR + "implicit/").c_str());
+        auto y_rk = four_stage_runge_kutta(grid, c_f, C_Y0, C_RK_STEPS[i], N);
 
-    //_mkdir(MOD_DIR.c_str());
-    //_mkdir((MOD_DIR + "modified/").c_str());
-    //_mkdir((MOD_DIR + "trapeze/").c_str());
-    //
-    //_mkdir(RK_DIR.c_str());
+        auto end = "h" + to_string(C_RK_STEPS[i]) + ".csv";
 
-    //// SIMPLE
-    //for (size_t i = 0; i < SIMPLE_TEST_NUM; i++)
-    //{
-    //    int N = (B - A) / simple_steps[i];
-    //    auto grid = build_grid(A, simple_steps[i], N);
-    //    auto as = analytical_solution(grid, solution);
-
-    //    auto y_simple = simple(grid, f, Y0, simple_steps[i], N);
-    //    auto y_implicit = implicit(grid, f, Y0, simple_steps[i], N, EPS, MAX_ITER);
-
-    //    auto end = "h" + to_string(simple_steps[i]) + ".csv";
-
-    //    ofstream out(SIMPLE_DIR + "simple/" + end);
-    //    out.imbue(locale(""));
-    //    dump_table(out, grid, y_simple, as);
-    //    out.close();
-
-    //    out.open(SIMPLE_DIR + "implicit/" + end);
-    //    dump_table(out, grid, y_implicit, as);
-    //    out.close();
-    //}
-    //
-    //// MODIFIED
-    //for (size_t i = 0; i < MOD_TEST_NUM; i++)
-    //{
-    //    int N = (B - A) / modified_steps[i];
-    //    auto grid = build_grid(A, modified_steps[i], N);
-    //    auto as = analytical_solution(grid, solution);
-
-    //    auto y_mod = modified(grid, f, Y0, modified_steps[i], N);
-    //    auto y_trapeze = trapeze(grid, f, Y0, modified_steps[i], N, EPS, MAX_ITER);
-
-    //    auto end = "h" + to_string(modified_steps[i]) + ".csv";
-
-    //    ofstream out(MOD_DIR + "modified/" + end);
-    //    out.imbue(locale(""));
-    //    dump_table(out, grid, y_mod, as);
-    //    out.close();
-
-    //    out.open(MOD_DIR + "trapeze/" + end);
-    //    dump_table(out, grid, y_trapeze, as);
-    //    out.close();
-    //}
-
-    //// RUNGE - KUTTA
-    //for (size_t i = 0; i < RK_TEST_NUM; i++)
-    //{
-    //    int N = (B - A) / rk_steps[i];
-    //    auto grid = build_grid(A, rk_steps[i], N);
-    //    auto as = analytical_solution(grid, solution);
-
-    //    auto y_rk = four_stage_runge_kutta(grid, f, Y0, rk_steps[i], N);
-
-    //    auto end = "h" + to_string(rk_steps[i]) + ".csv";
-
-    //    ofstream out(RK_DIR + end);
-    //    out.imbue(locale(""));
-    //    dump_table(out, grid, y_rk, as);
-    //    out.close();
-    //}
+        ofstream out(RK_DIR + end);
+        out.imbue(locale(""));
+        dump_table(out, grid, y_rk, as);
+        out.close();
+    }
+#pragma endregion
 
     return 0;
 }
